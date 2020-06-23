e4c4594a13383bd93fdf479057b1827f60c3f86e
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2020-06-23 19:07:33 +0100

    swap icon pack categories

--- app/src/main/java/com/ibile/core/Application.kt
+++ app/src/main/java/com/ibile/core/Application.kt
@@ -31,7 +31,23 @@ class Application : Application() {
         val loader = IconPackLoader(this)
         val defaultIconPack = createDefaultIconPack(loader)
         val pack = loader.load(R.xml.extra_map_icons, 0, defaultIconPack.locales, defaultIconPack)
+
+        val parent = pack.parent!!
+        val familyPeopleCategory = parent.categories[0]!!
+        val buildingPlacesCategory = parent.categories[7]!!
+        parent.categories[0] = buildingPlacesCategory
+        parent.categories[7] = familyPeopleCategory
+        val icons = parent.icons
+        icons.forEach { (key, icon) ->
+            if (icon.categoryId == 0 || icon.categoryId == 7) {
+                val newCategoryId: Int = if (icon.categoryId == 0) 7 else 0
+                val iconModified = icon.copy(categoryId = newCategoryId)
+                icons[key] = iconModified
+            }
+        }
+
         pack.loadDrawables()
+
         return pack
     }
 }
